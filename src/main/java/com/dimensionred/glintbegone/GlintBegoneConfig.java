9fe528f7bfa385fca10bce97d624500fa9d60b99
DimensionRed <redbird7844@gmail.com>
2025-05-26 20:47:17 +0500

    New NbtList

--- src/main/java/com/dimensionred/glintbegone/GlintBegoneConfig.java
+++ src/main/java/com/dimensionred/glintbegone/GlintBegoneConfig.java
@@ -1,10 +1,8 @@
 package com.dimensionred.glintbegone;
 
-import me.shedaniel.autoconfig.AutoConfig;
+
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 
 import java.util.ArrayList;
 import java.util.List;
